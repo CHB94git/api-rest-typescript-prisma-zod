555a6fdedf57d9d5a9df2999c97b25b0ba70c25c
Cristian Hernandez <christianhb534@gmail.com>
2023-01-24 18:27:19 -0500

    Ajustes middleware session and edit script dev package.json

--- src/middlewares/check-session.ts
+++ src/middlewares/check-session.ts
@@ -1,5 +1,4 @@
 import { NextFunction, Request, Response } from 'express';
-// import { RequestExt } from '../interfaces/req-extended';
 import { verifyToken } from '../utils/handle.jwt';
 
 interface TokenErrors {
@@ -10,16 +9,17 @@ const tokenVerificationErrors: TokenErrors = {
   ["jwt must be provided"]: "Debe de proporcionar un jsonwebtoken - Bearer Token",
   ["invalid signature"]: "Firma no válida del token",
   ["jwt malformed"]: "Formato inválido del token - token defectuoso",
-  ["jwt expired"]: "El jsonwebtoken ha expirado"
+  ["jwt expired"]: "El jsonwebtoken ha expirado",
+  ["invalid token"]: "Token no válido"
 }
 
 export const checkAuthStatus = async (request: Request, response: Response, next: NextFunction) => {
   try {
-    if (!request.headers.authorization)
-      return response.status(400).send()
+    if (request.headers.authorization === undefined)
+      throw new Error('jwt must be provided')
 
     const jwt = request.headers.authorization
-    const token = jwt.split(' ').pop()!
+    const token = jwt.split(' ').pop()
 
     const isAuth = await verifyToken(`${ token }`) as { uid: string }
     request.user = isAuth
